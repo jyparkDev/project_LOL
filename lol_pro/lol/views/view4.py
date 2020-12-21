1f7f284be8b9eec0cd6edea02216206a599d43a7
hee jae <hee jae@DESKTOP-KO88MC0>
2020-12-21 16:09:25 +0900

    승리예측 결과 출력..

--- lol_pro/lol/views/view4.py
+++ lol_pro/lol/views/view4.py
@@ -5,14 +5,27 @@ import pickle
 from tensorflow.keras.models import load_model
 import random
 import os.path
+import numpy as np
+
 def QueryFunc(request):
-#     blue1 = request.GET['blue1']
+    return render(request, 'multi/query.html')
+
+def RequeryFunc(request):
+    blue1 = request.POST.get('blue1')
+    blue2 = request.POST.get('blue2')
+    blue3 = request.POST.get('blue3')
+    blue4 = request.POST.get('blue4')
+    blue5 = request.POST.get('blue5')
     
+    purple1 = request.POST.get('purple1')
+    purple2 = request.POST.get('purple2')
+    purple3 = request.POST.get('purple3')
+    purple4 = request.POST.get('purple4')
+    purple5 = request.POST.get('purple5')
     
-#     print(blue1)
     path = os.path.abspath(os.path.dirname(__file__))
-    rf = pickle.load(open(path + '/model/randomforest_indi.sav', 'rb'))
-    model = load_model(path + '/model/sequential_indi.hdf5')
+    rf = pickle.load(open(path + '/model/randomforest_indi.sav', 'rb')) # 랜덤포레스트 모델
+    model = load_model(path + '/model/sequential_indi.hdf5') # 시퀀셜 모델
     
 
     
@@ -22,41 +35,43 @@ def QueryFunc(request):
     
     sample_summoners = random.sample(summoners_name, 10)
     
-    blues = random.sample(sample_summoners, 5)
-    reds = list(set(sample_summoners) - set(blues))
+#     blues = random.sample(sample_summoners, 5)
+#     reds = list(set(sample_summoners) - set(blues))
     
-    blue_team_summoners = blues
-    red_team_summoners = reds
+#     blue_team_summoners = blues
+#     red_team_summoners = reds
+    blue_team_summoners = [blue1, blue2, blue3, blue4, blue5]
+    red_team_summoners = [purple1, purple2, purple3, purple4, purple5]
     print(blue_team_summoners)
     print(red_team_summoners)
-    # blue_team_summoners = ['16xyz', 'HUYATV SPARKLE', '북쪽의 괴물1', '충주갱수', 'daydayup']
-    # red_team_summoners = ['Shining fans', 'sofm2', 'Liiv Route', 'FA Natalie', '모찌피치모찌피치']
     
-    blue_df = pd.DataFrame()
-    red_df = pd.DataFrame()
+    blue_df1 = pd.DataFrame()
+    red_df1 = pd.DataFrame()
     for summoners in blue_team_summoners:
         imsi = ten_summoners_df[ten_summoners_df['summonerName'] == summoners]
-        blue_df = pd.concat([blue_df, imsi])
+        blue_df1 = pd.concat([blue_df1, imsi])
     for summoners in red_team_summoners:
         imsi = ten_summoners_df[ten_summoners_df['summonerName'] == summoners]
-        red_df = pd.concat([red_df, imsi])
+        red_df1 = pd.concat([red_df1, imsi])
     
-    blue_df = blue_df.reset_index(drop=True)
-    red_df = red_df.reset_index(drop=True)
+    blue_df1 = blue_df1.reset_index(drop=True)
+    red_df1 = red_df1.reset_index(drop=True)
+    blue = round(blue_df1, 2).to_dict(orient='record')
+    purple = round(red_df1, 2).to_dict(orient='record')
     
-    # print(blue_df.iloc[:, 4:])
-    # print(red_df.iloc[:, 4:])
+    print(blue_df1)
+    print(red_df1)
     
-    print(list(blue_df.iloc[:, 4:].columns))
+    print(list(blue_df1.iloc[:, 4:].columns))
     
-    col_sum = list(blue_df.iloc[:, 4:].columns)
+    col_sum = list(blue_df1.iloc[:, 4:].columns)
     col_mean = ['firstBloodKill', 'firstInhibitorKill', 'visionScore', 'kda'] # ☆'firstBloodKill', 'firstInhibitorKill'는 평균이 나을까?
     for dele in col_mean:
         col_sum.remove(dele)
     
-    blue_total = blue_df[col_sum].sum().append(blue_df[col_mean].mean()).to_list()
+    blue_total = blue_df1[col_sum].sum().append(blue_df1[col_mean].mean()).to_list()
     print(blue_total)
-    red_total = red_df[col_sum].sum().append(red_df[col_mean].mean()).to_list()
+    red_total = red_df1[col_sum].sum().append(red_df1[col_mean].mean()).to_list()
     print(red_total)
     
     blue_total.extend(red_total)
@@ -64,9 +79,9 @@ def QueryFunc(request):
     # both_df = pd.concat([ten_fin_df_blue, ten_fin_df_red], axis=1)
     
     
-    # pred = model.predict([blue_total])
+    pred = model.predict([blue_total])
     # pred = (model.predict([blue_total]) > 0.5).astype('int32')
-    pred = rf.predict([blue_total])
+#     pred = rf.predict([blue_total])
     # pred = (rf.predict([blue_total]) > 0.5).astype('int32')
     print('예측값:', pred.flatten()) # blue team (100) 기준
         
@@ -82,12 +97,17 @@ def QueryFunc(request):
     summoners_name = list(ten_summoners_df['summonerName'])
     
     sample_summoners = random.sample(summoners_name, 10)
-    
+    """
     blues = random.sample(sample_summoners, 5)
     reds = list(set(sample_summoners) - set(blues))
     
     blue_team_summoners = blues
     red_team_summoners = reds
+    print(blue_team_summoners)
+    print(red_team_summoners)
+    """
+    blue_team_summoners = [blue1, blue2, blue3, blue4, blue5]
+    red_team_summoners = [purple1, purple2, purple3, purple4, purple5]
     print(blue_team_summoners)
     print(red_team_summoners)
     # blue_team_summoners = ['16xyz', 'HUYATV SPARKLE', '북쪽의 괴물1', '충주갱수', 'daydayup']
@@ -114,10 +134,23 @@ def QueryFunc(request):
     print(both_team_mean_series)
     print(both_team_mean_list)
     
-    # pred = model.predict([zboth_team_mean_list])
-    # pred = (model.predict([both_team_mean_list]) > 0.5).astype('int32')
-    pred = rf.predict([both_team_mean_list])
-    # pred = (rf.predict([both_team_mean_list]) > 0.5).astype('int32')
-    print('예측값:', pred.flatten()) # blue team (100) 기준    
+#     pred = model.predict([both_team_mean_list])
     
-    return render(request, 'multi/query.html')
+#     pred = (model.predict([both_team_mean_list]) > 0.5).astype('int32')
+#     pred = rf.predict([both_team_mean_list])
+#     pred = (rf.predict([both_team_mean_list]) > 0.5).astype('int32')
+    
+    key1 = ""
+    key2 = ""
+    key3 = ""
+    if pred > 0.5:
+        key1 = "블루팀 승리"
+        key2 = "퍼플팀 패배"
+        key3 = "블루팀이 " + str(round(pred[0][0]*100,2)) + "% 확률로 승리예측"
+    else:
+        key1 = "블루팀 패배"
+        key2 = "퍼플팀 승리"
+        key3 = "퍼플팀 승리 " + str(round((1 - pred[0][0])*100,2)) + "% 확률로 승리예측"
+    print('예측값:', pred.flatten()) # blue team (100) 기준   
+    print(key3)
+    return render(request, 'multi/query.html', {'key1':key1, 'key2':key2, 'key3':key3 ,'blue':blue, 'purple':purple, 'pred':pred})
